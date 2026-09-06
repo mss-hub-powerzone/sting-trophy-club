1f6152f44e0b1bb968b791db761926a2cc33aa0b
mss-hub-powerzone <jon.barber@mssco.com>
2026-09-05 19:12:55 -0500

    Add Zach Morris U17 captain photo

--- app/page.tsx
+++ app/page.tsx
@@ -293,7 +293,7 @@ export default async function Home() {
           <div className="captainGrid">
             <article className="captainPhoto"><img src="/captains/evan.jpg" alt="Evan John, U17 Sting captain" /><span>U17</span><h3>Evan</h3><small>CAPTAIN</small></article>
             <article className="captainPhoto kyanPhoto"><img src="/captains/kyan.png" alt="Kyan, U17 Sting captain" /><span>U17</span><h3>Kyan</h3><small>CAPTAIN</small></article>
-            <article><span>U17</span><h3>Zach</h3><small>CAPTAIN</small></article>
+            <article className="captainPhoto zachPhoto"><img src="/captains/zach-morris.jpg" alt="Zach Morris, U17 Sting captain" /><span>U17</span><h3>Zach</h3><small>CAPTAIN</small></article>
             <article className="u16 captainPhoto"><img src="/captains/sawyer.jpg" alt="Sawyer, U16 Sting captain" /><span>U16</span><h3>Sawyer</h3><small>CAPTAIN</small></article>
             <article className="u16 captainPhoto"><img src="/captains/logan.jpg" alt="Logan, U16 Sting captain" /><span>U16</span><h3>Logan</h3><small>CAPTAIN</small></article>
             <article className="u16 captainPhoto"><img src="/captains/isaac.jpg" alt="Isaac, U16 Sting captain" /><span>U16</span><h3>Isaac</h3><small>CAPTAIN</small></article>
